986a0d6c266d176064fed217769d2b1acbe8d9a6
Iannes <yannispyrou@gmail.com>
2019-08-13 11:05:16 +0100

    Success text change

--- src/pages/success.js
+++ src/pages/success.js
@@ -48,7 +48,7 @@ const Success = () => (
         <Link to="/">{config.siteTitle}</Link>
       </Header>
       <Content>
-        <h1>Your message was successfully sent!</h1>
+        <h1>Your message was sent!</h1>
         <p>I'll answer your request as quick as possible.</p>
       </Content>
     </Wrapper>
