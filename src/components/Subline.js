8b2d874ab774809a565b3f619af96c17a71fe0c3
Iannes <yannispyrou@gmail.com>
2019-08-17 21:59:52 +0100

    add subline

--- src/components/Subline.js
+++ src/components/Subline.js
@@ -2,10 +2,12 @@ import styled from 'styled-components'
 
 const Subline = styled.article`
   font-size: ${props => props.theme.fontSize.small};
-  color: ${props => props.theme.colors.grey.light};
+  color: ${props => props.theme.colors.grey.light};  
   ${props => props.sectionTitle && 'margin-top: -3rem'};
   ${props => props.sectionTitle && 'margin-bottom: 4rem'};
   ${props => props.sectionTitle && 'text-align: center'};
+  ${props => props.date && 'margin-bottom: -15px'};
+  ${props => props.date && 'line-height: -10px'};
 `
 
 export default Subline
