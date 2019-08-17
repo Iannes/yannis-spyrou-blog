0edbd4a0ddf82b7b66fb56e220f1fb1e320b000f
Iannes <yannispyrou@gmail.com>
2019-08-17 11:19:15 +0100

    changes

--- config/index.js
+++ config/index.js
@@ -9,8 +9,8 @@ module.exports = {
   siteHeadline: 'Writing and publishing tech content', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Yannis Spyrou is a frontend developer, working with TypeScript / React & AWS Serverless.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'Frontend developer | Serverless Advocate | Music Lover.', // Your site description
+  author: 'Iannes', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
