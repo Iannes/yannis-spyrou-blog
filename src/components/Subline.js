06427d8db856985a5406db8b80c3c8ead9e4dd82
Iannes <yannispyrou@gmail.com>
2019-08-18 14:25:11 +0100

    add condition in styled components

--- src/components/Subline.js
+++ src/components/Subline.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Subline = styled.article`
   font-size: ${props => props.theme.fontSize && props.theme.fontSize.small};
-  color: ${props => props.theme.colors.grey.light};  
+  color: ${props => props.theme.colors && props.theme.colors.grey.light};  
   ${props => props.sectionTitle && 'margin-top: -3rem'};
   ${props => props.sectionTitle && 'margin-bottom: 4rem'};
   ${props => props.sectionTitle && 'text-align: center'};
