0444768c9fa5d1bb5669ce3b2297e849806ca909
Iannes <yannispyrou@gmail.com>
2019-08-21 20:15:39 +0100

    propchecks

--- src/components/SimpleTitle.js
+++ src/components/SimpleTitle.js
@@ -4,7 +4,7 @@ const SimpleTitle = styled.h3`
   font-size: ${props => props.theme && props.theme.fontSize && props.theme.fontSize.small};
   text-transform: capitalize;
   font-weight: bold;  
-  color: ${props => props.theme.colors.grey.dark};  
+  color: ${props => props.theme && props.theme.colors && props.theme.colors.grey.dark};  
   padding: 0;  
   margin: 0;  
 `
