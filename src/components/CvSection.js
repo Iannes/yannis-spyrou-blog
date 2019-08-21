3a030a6b6c4828871bd1dfe6eaee4ea6af304e38
Iannes <yannispyrou@gmail.com>
2019-08-21 08:57:31 +0100

    styles: padding change in about page

--- src/components/CvSection.js
+++ src/components/CvSection.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const CvSection = styled.article`  
-  ${props => props.role && 'padding: 20px 0'};  
+  ${props => props.role && 'padding: 10px 0'};  
   ${props => props.role && 'font-weight: bold'};  
   ${props => props.highlight && 'background: #fff9f9'};  
   ${props => props.highlight && 'border-radius: 5px'};  
