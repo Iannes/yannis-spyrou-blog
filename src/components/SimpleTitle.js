1da30bdde25200c00a67786bb9138d6a3eb6f546
Iannes <yannispyrou@gmail.com>
2019-08-21 16:59:41 +0100

    error on theme

--- src/components/SimpleTitle.js
+++ src/components/SimpleTitle.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const SimpleTitle = styled.h3`
-  font-size: ${props => props.theme.fontSize.medium};
+  font-size: ${props => props.theme.fontSize.small};
   text-transform: capitalize;
   font-weight: bold;  
   color: ${props => props.theme.colors.grey.dark};  
