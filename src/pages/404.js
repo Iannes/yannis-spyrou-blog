faab18826288705ea9cc50f124d1431e75ac4d5e
Iannes <yannispyrou@gmail.com>
2019-08-24 20:23:46 +0100

    tweak 404 message

--- src/pages/404.js
+++ src/pages/404.js
@@ -37,7 +37,7 @@ const NotFoundPage = () => (
           <Link to="/">{config.siteTitle}</Link>
         </Header>
         <Hero>
-          <h2>Oops!</h2>
+          <h2>Eeer... What happened now?</h2>
           <Iris />
           <Iris />
           <p>Looks like you got lost! Click on the button below to head over to the home page.</p>
