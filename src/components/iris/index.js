14fdbf064b96fb07ea22581e0a9cd7d0bf7f1689
Iannes <yannispyrou@gmail.com>
2019-08-24 20:10:17 +0100

    add 404

---
+++ src/components/iris/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './iris.css';
+
+const Iris = () => (
+  <section className="container">
+    <section className="stage">
+      <figure className="ball">
+        <span className="shadow" />
+        <span className="iris" />
+      </figure>
+    </section>
+  </section>
+);
+
+export default Iris;
