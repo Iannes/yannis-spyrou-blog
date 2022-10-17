d23b02e714213ec1ff89b468213bbed6c2dbe2e0
Yannis Spyrou <yannispyrou@gmail.com>
2022-10-17 11:10:13 +0200

    migrate to nextjs

--- src/components/Wrapper.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const Wrapper = styled.section`display: grid; grid-template-columns: 1fr minmax(280px, ${props => props.theme.maxWidth}) 1fr; padding: 0 1rem; @media (max-width: 767px) { padding: 0; }`;
-
-export default Wrapper;
---
+++ src/data/next-seo.config.ts
@@ -0,0 +1,25 @@
+const seoConfig = {
+  title: 'Yannis Spyrou',
+  description: 'I’m Yannis and I’m a Frontend engineer!',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://yannisspyrou.com/',
+    site_name: 'Yannis Spyrou Blog',
+    images: [
+      {
+        url: 'https://yannisspyrou.com/assets/thumbnail/thumbnail.png',
+        width: 1200,
+        height: 600,
+        alt: 'I’m Yannis and I’m a Frontend engineer!',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@SpyrouYannis',
+    site: '@SpyrouYannis',
+    cardType: 'summary_large_image',
+  },
+};
+
+export default seoConfig;
